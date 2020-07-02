adf178dabbcbd0f35748a23d1f499986deb913dd
Kelsey Myers <52179263+kelsey-myers@users.noreply.github.com>
2020-07-02 21:35:15 +0100

    Updates

---
+++ precache-manifest.9e59cd2bf8316023bbbbdc4e238e687f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa5863d7e67e9166d75f",
+    "url": "/react-todo-list/static/css/main.bcb0589b.chunk.css"
+  },
+  {
+    "revision": "fa5863d7e67e9166d75f",
+    "url": "/react-todo-list/static/js/main.f2b5d2da.chunk.js"
+  },
+  {
+    "revision": "89737d6b5129f89a8a0b",
+    "url": "/react-todo-list/static/js/runtime~main.eb2f80b6.js"
+  },
+  {
+    "revision": "70793e2910c1b4447a4d",
+    "url": "/react-todo-list/static/css/2.def015fa.chunk.css"
+  },
+  {
+    "revision": "70793e2910c1b4447a4d",
+    "url": "/react-todo-list/static/js/2.30225597.chunk.js"
+  },
+  {
+    "revision": "1d0751390d44c22a277cee4a9203186d",
+    "url": "/react-todo-list/static/media/background.1d075139.jpg"
+  },
+  {
+    "revision": "39b032e2e44daf83b12f4dc81b147453",
+    "url": "/react-todo-list/index.html"
+  }
+];
